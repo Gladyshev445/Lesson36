bd1959a4ea2cc8fd3e94101fb1213ff1515cacc0
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-03-01 19:41:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+//let a = prompt("Число")
+//if (a == 10){
+ //   alert("Верно")
+//}
+//else {
+ //   alert("Неверно")
+//}
+
+//let a = prompt("Число")
+//if (a < 4){
+//    alert("Первая часть")
+//}
+//else if (a < 7){
+ //   alert("Вторая часть")
+//}
+//else if (a < 10){
+ //   alert("Третья часть")
+//}
+//else if (a < 13){
+//    alert("Четвертая часть")
+//}
+//else{
+ //   alert("Нет других")
+//}
+
+//let x = prompt("Введите long")
+//if (x == "long"){
+ //   let y = prompt("Введите en или чи")
+ //   if (y == "чи"){
+ //       alert("В недели 7 дней")
+ //   }
+ //   else if (y == "en"){
+ //       alert("In weekend have is seven days")
+ //   }
+ //   else if (!(y == "чи" || y == "en")){
+ //       alert("Вы не ввели заданые слова")
+ //   }
+//}
+
+
+//    let arr = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+//alert(arr[5])
+
+//let x = prompt("Введите язык")
+//let y = arr =["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+//    z = arr = ["Mn", "Te", "Sr", "Fo", "Fr", "rrr", "We"]
+//if (x == "ру"){
+//    alert(y)
+//}
+//else if (x == "en"){
+//    alert(z)
+//}
+
+let a = Number = prompt("Введите первое число")
+    b = Number = prompt("Введите второе число")
+
+if (a <= 1 || b >= 3){
+    alert(a + b)
+}
+else if (!a || b){
+    alert("Разность чисел" + a && b)
+}
